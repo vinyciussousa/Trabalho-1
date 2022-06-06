bd9616e4551337ea108b7c028ec5707de6ae2895
Marcos Vinycius Soares de Sousa <marcosvinyciusss@gmail.com>
2022-06-06 14:18:01 +0000

    Otimizações e inicio da ordenação utilizando heap.
    Co-authored-by: Pedro Henrique Duarte Monteiro <pedro.monteiro66@yahoo.com.br>

--- sqrtSort.py
+++ sqrtSort.py
@@ -1,6 +1,7 @@
 from math import floor
 import scipy as sp
 import numpy as np
+import heapq as hp
 import random
 import time
 
@@ -23,11 +24,17 @@ def listAllocation(lista, tamanho):
     return partes
 
 def bubble_sort(lista):
-    for i in range(len(lista)):
-        for j in range(len(lista) - 1):
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
-
+  for i in range(len(lista)):
+    trocou = False
+    for j in range(0, len(lista) - i - 1):
+      if lista[j] > lista[j + 1]:
+        temp = lista[j]
+        lista[j] = lista[j+1]
+        lista[j+1] = temp
+        trocou = True
+    if not trocou:
+      break
+        
 def ordena_bubbleSort(lista, tamanho):
     resultado = []
     maiores = []
@@ -46,18 +53,43 @@ def ordena_bubbleSort(lista, tamanho):
     resultado.reverse()
     return resultado
 
+
+def ordena_heap(lista,tamanho):
+	maiores = []
+	resultado = []
+	print(lista)
+	for i in range(len(lista)):
+		hp._heapify_max(lista[i])
+		hp.heappush(maiores, hp._heappop_max(lista[i]))
+	for i in range(tamanho):
+		hp.heappush(resultado,hp._heappop_max(maiores))
+	print(lista)
+	print(maiores)
+    print(resultado)
+	#for que vai passar pelas heaps pegando os maiores
+	#for que vai tirar o maior(ou menor) da heap de maiores para a heap de resultados
+	#trazer o maior da heaps de parte de onde o maior saiu
+	#repeat?
+
+ 
 if __name__ == '__main__':
     entrada = [4, 6, 1, 19, 125, 5968, 26, 760, 16, 2, 3, 4, 3]
     entrada_random = random.sample(range(0,100000), 100000)
     #print(entrada_random)
-    tamanho = len(entrada_random)
+    tamanho = len(entrada)
+
+		
+    #start = time.time()
+    #partes = listAllocation(entrada_random, tamanho)
+    #resultado = ordena_bubbleSort(partes, tamanho)
+    #end = time.time()
+    #print(resultado)
+    #print(end - start)
 
     start = time.time()
-    partes = listAllocation(entrada_random, tamanho)
-    resultado = ordena_bubbleSort(partes, tamanho)
+    partes = listAllocation(entrada, tamanho)
+    ordena_heap(partes, tamanho)
     end = time.time()
-    print(resultado)
-    print(end - start)
 
 #print(round(np.sqrt(tamanho))) # Tamanho das partes
 #print((tamanho % floor(np.sqrt(tamanho)))) # Tamanho da última parte
